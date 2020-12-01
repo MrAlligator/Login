6b4a6bd553a26628c11310f771ff1fef9beca5db
Rizki Widya Pratama <rizkiw8778@gmail.com>
2020-12-01 17:03:42 +0700

    new login

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,57 +1,19 @@
-<!-- Begin Page Content -->
-<div class="container-fluid">
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
-    <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
-
-</div>
-<!-- /.container-fluid -->
-
-</div>
-<!-- End of Main Content -->
-
-<!-- End of Footer -->
-
-</div>
-<!-- End of Content Wrapper -->
-
-</div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
-
-<!-- Logout Modal-->
-<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-            <div class="modal-footer">
-                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="<?= base_url('auth/logout'); ?>">Logout</a>
-            </div>
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800"><?= $title ?></h1>
         </div>
-    </div>
-</div>
 
-<!-- Bootstrap core JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jquery/jquery.min.js"></script>
-<script src="<?= base_url('assets/'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Content Row -->
+        <div class="row">
 
-<!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+        </div>
 
-<!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets/'); ?>js/sb-admin-2.min.js"></script>
 
-</body>
+        </div>
+        <!-- /.container-fluid -->
 
-</html>
+        </div>
+        <!-- End of Main Content -->
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,72 +1,66 @@
 <!-- Sidebar -->
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-    <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon">
-            <i class="fas fa-user"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Admin</div>
-    </a>
+<!-- Sidebar - Brand -->
+<a class="sidebar-brand d-flex align-items-center justify-content-center" href="">
+  <div class="sidebar-brand-icon">
+    <i class="fas fa-address-card"></i>
+  </div>
+  <div class="sidebar-brand-text mx-3">ICSB</div>
+</a>
 
-    <!-- Divider -->
-    <hr class="sidebar-divider ">
+<!-- Divider -->
+<hr class="sidebar-divider">
 
-    <!-- Heading -->
+<?php 
+    $role_id = $this->session->userdata('role_id');
+    $queryMenu = "SELECT user_menu.id, menu FROM user_menu JOIN user_access_menu ON user_menu.id = user_access_menu.menu_id WHERE user_access_menu.role_id = $role_id ORDER BY user_access_menu.menu_id ASC";
+    $menu = $this->db->query($queryMenu)->result_array();
+?>
+
+<?php foreach ($menu as $m) : ?>
     <div class="sidebar-heading">
-        Admin
+        <?= $m['menu']; ?>
     </div>
 
-    <!-- Nav Item - Dashboard -->
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url(''); ?>">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
-            <span>Dashboard</span></a>
-    </li>
+    <?php 
+    $menuId = $m['id'];
+    $querySubMenu = "SELECT * FROM user_sub_menu WHERE menu_id = $menuId AND is_active = '1'";
+    $subMenu = $this->db->query($querySubMenu)->result_array();
+    ?>
 
-    <!-- Divider -->
+    <?php foreach ($subMenu as $sm) : ?>
+        <li class="nav-item">
+            <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+                <i class="<?php echo $sm['icon'] ?>"></i>
+            <span><?= $sm['title'] ?></span></a>
+        </li>
+    <?php endforeach; ?>
+
     <hr class="sidebar-divider">
 
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        User
-    </div>
+<?php endforeach; ?>
 
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user') ?>">
-            <i class="far fa-fw fa-id-card"></i>
-            <span>My Profile</span></a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user/edit'); ?>">
-            <i class="far fa-fw fa-id-card"></i>
-            <span>Edit Profile</span></a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('user/changePassword'); ?>">
-            <i class="far fa-fw fa-key"></i>
-            <span>Edit Password</span></a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <li class="nav-item">
-        <a class="nav-link" href="<?= base_url('auth/logout') ?>">
-            <i class="fas fa-fw fa-sign-out-alt"></i>
-            <span>Logout</span></a>
-    </li>
+<!-- Nav Item - Dashboard -->
 
 
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+<!-- Divider -->
 
-    <!-- Sidebar Toggler (Sidebar) -->
-    <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
-    </div>
+
+<!-- Nav Item - Tables -->
+<li class="nav-item">
+  <a class="nav-link" href="<?= base_url('auth/logout')  ?>">
+    <i class="fas fa-fw fa-sign-out-alt"></i>
+    <span>Logout</span></a>
+</li>
+
+<!-- Divider -->
+<hr class="sidebar-divider d-none d-md-block">
+
+<!-- Sidebar Toggler (Sidebar) -->
+<div class="text-center d-none d-md-inline">
+  <button class="rounded-circle border-0" id="sidebarToggle"></button>
+</div>
 
 </ul>
 <!-- End of Sidebar -->
